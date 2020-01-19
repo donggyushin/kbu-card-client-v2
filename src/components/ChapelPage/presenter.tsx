54460f824c228b7bf4ce7c71c274dc6feafdeccf
shin donggyu <donggyu9410@gmail.com>
2020-01-19 18:18:12 +0900

    location bug fixed

--- src/components/ChapelPage/presenter.tsx
+++ src/components/ChapelPage/presenter.tsx
@@ -1,12 +1,29 @@
-import React from 'react'
+import React, { Dispatch, useEffect } from 'react'
 import styled from 'styled-components'
 import Navigation from '../NavigationBar'
 import BottomNavigation from '../BottomNavigation'
 import ChapelBody from './Body'
+import { useDispatch } from 'react-redux'
+import { UPDATE_CURRENT_LOCATION } from '../../actions/types.d'
 
 const Container = styled.div``
 
+interface UpdateCurrentLocationDispatch {
+    type: string
+    current: string
+}
+
 const Presenter: React.FC = () => {
+
+    const locationDispatch = useDispatch<Dispatch<UpdateCurrentLocationDispatch>>()
+
+    useEffect(() => {
+        locationDispatch({
+            type: UPDATE_CURRENT_LOCATION,
+            current: 'chapel'
+        })
+    }, [])
+
     return <Container>
         <Navigation />
         <ChapelBody />

48bdd1d951b2d52958013f3f46bcb6f84aafa346
shin donggyu <donggyu9410@gmail.com>
2020-02-07 02:03:52 +0900

    test react helmet

--- src/components/MainComponent/presenter.tsx
+++ src/components/MainComponent/presenter.tsx
@@ -3,6 +3,7 @@ import styled from 'styled-components'
 import Navigation from '../NavigationBar'
 import BottomNavigation from '../BottomNavigation'
 import Body from './Body'
+import { Helmet } from 'react-helmet'
 
 const Container = styled.div`
     display:flex;
@@ -13,6 +14,10 @@ const Container = styled.div`
 
 const MainComponentPresenter: React.FC = () => {
     return <Container>
+        <Helmet>
+            <title>성서봇</title>
+            <meta name="description" content="한국성서대학교 공식 모바일 전용 웹 어플리케이션" />
+        </Helmet>
         <Navigation />
         <Body />
         <BottomNavigation />

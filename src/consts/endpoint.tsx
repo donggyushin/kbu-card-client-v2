fef0a719ee27e1aa4ba8424b4ca58f4006fcdd60
shin donggyu <donggyu9410@gmail.com>
2020-02-06 09:09:16 +0900

    change endpoint url to https

--- src/consts/endpoint.tsx
+++ src/consts/endpoint.tsx
@@ -1,10 +1,10 @@
-export const END_POINT = 'http://220.67.154.77:9766/'
-export const END_POINT_TEST = 'http://220.67.154.77:9766/'
+export const END_POINT = 'https://kbucard.com:9766/'
+export const END_POINT_TEST = 'https://kbucard.com:9766/'
 export let DONGGYU_END_POINT = 'http://localhost:4001/api/'
 
 if (!process.env.NODE_ENV || process.env.NODE_ENV === 'development') {
     // dev code
 } else {
     // production code
-    DONGGYU_END_POINT = 'http://220.67.154.77:4001/api/'
+    DONGGYU_END_POINT = 'https://kbucard.com:4001/api/'
 }

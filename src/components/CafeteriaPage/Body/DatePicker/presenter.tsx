4512734d3526ef290a11db968a4e543c92f49f60
shin donggyu <donggyu9410@gmail.com>
2020-02-07 03:01:48 +0900

    custom datepicker

--- src/components/CafeteriaPage/Body/DatePicker/presenter.tsx
+++ src/components/CafeteriaPage/Body/DatePicker/presenter.tsx
@@ -11,12 +11,25 @@ import {
     fetchMenu
 } from '../../../../actions/cafeteriaForPage'
 import { useDispatch } from 'react-redux';
+import styled from 'styled-components'
+
+const DateDiv = styled.div`
+    position: absolute;
+    top: 22px;
+    font-size: 13px;
+    font-weight: 600;
+    right: 48%;
+`
 
 export default function MaterialUIPickers() {
     // The first commit of Material-UI
     const [selectedDate, setSelectedDate] = React.useState<Date | null>(
         new Date(),
     );
+    const [year, setYear] = React.useState(2020)
+    const [month, setMonth] = React.useState(0)
+    const [day, setDay] = React.useState(0)
+    const [dayName, setDayName] = React.useState("")
 
     const fetchingMenuDispatch = useDispatch<Dispatch<IFetchMenuDispatch>>()
 
@@ -33,12 +46,16 @@ export default function MaterialUIPickers() {
 
     useEffect(() => {
         callFetchingMenuApi()
+        setYear(selectedDate?.getFullYear() || 2020)
+        setMonth(selectedDate?.getMonth() || 0)
+        setDay(selectedDate?.getDate() || 0)
+        getDayName()
     }, [selectedDate])
 
     return (
         <MuiPickersUtilsProvider utils={DateFnsUtils}>
-            <Grid container justify="space-around">
-
+            <Grid className="datepicker-container" container justify="space-around">
+                <DateDiv>{year}년 {month}월 {day}일 {dayName}요일</DateDiv>
                 <KeyboardDatePicker
                     margin="normal"
                     id="date-picker-dialog"
@@ -54,6 +71,12 @@ export default function MaterialUIPickers() {
             </Grid>
         </MuiPickersUtilsProvider>
     );
+
+    function getDayName(): void {
+        const week = ['일', '월', '화', '수', '목', '금', '토']
+        const dayOfWeek = week[selectedDate?.getDay() || 0];
+        setDayName(dayOfWeek)
+    }
 
     function callFetchingMenuApi() {
         if (selectedDate) {
--- src/components/TodayPray/Body/DatePicker/presenter.tsx
+++ src/components/TodayPray/Body/DatePicker/presenter.tsx
@@ -11,26 +11,49 @@ import {
     getTodayPray
 } from '../../../../actions/todayPray'
 import { useDispatch } from 'react-redux';
+import styled from 'styled-components'
+
+const DateDiv = styled.div`
+    position: absolute;
+    top: 22px;
+    font-size: 13px;
+    font-weight: 600;
+    right: 48%;
+`
 
 export default function MaterialUIPickers() {
     // The first commit of Material-UI
     const [selectedDate, setSelectedDate] = React.useState<Date | null>(
         new Date(),
     );
+    const [year, setYear] = React.useState(2020)
+    const [month, setMonth] = React.useState(0)
+    const [day, setDay] = React.useState(0)
+    const [dayName, setDayName] = React.useState("")
 
     const handleDateChange = (date: Date | null) => {
         setSelectedDate(date);
     };
 
     useEffect(() => {
+        const input = document.getElementById("date-picker-dialog") as HTMLInputElement
+        input.disabled = true
+    }, [])
+
+    useEffect(() => {
         callFetchingTodayPray()
+        setYear(selectedDate?.getFullYear() || 2020)
+        setMonth(selectedDate?.getMonth() || 0)
+        setDay(selectedDate?.getDate() || 0)
+        getDayName()
     }, [selectedDate])
 
     const getTodayPrayDispatch = useDispatch<Dispatch<IGetTodayPrayDispatch>>()
 
     return (
         <MuiPickersUtilsProvider utils={DateFnsUtils}>
-            <Grid container justify="space-around">
+            <Grid className="datepicker-container" container justify="space-around">
+                <DateDiv>{year}년 {month}월 {day}일 {dayName}요일</DateDiv>
                 <KeyboardDatePicker
                     margin="normal"
                     id="date-picker-dialog"
@@ -45,6 +68,12 @@ export default function MaterialUIPickers() {
             </Grid>
         </MuiPickersUtilsProvider>
     );
+
+    function getDayName(): void {
+        const week = ['일', '월', '화', '수', '목', '금', '토']
+        const dayOfWeek = week[selectedDate?.getDay() || 0];
+        setDayName(dayOfWeek)
+    }
 
     function callFetchingTodayPray() {
         if (selectedDate) {

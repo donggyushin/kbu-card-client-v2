7ba5510c7c4da16a2753817315586e502067e6d2
shin donggyu <donggyu9410@gmail.com>
2020-02-12 19:57:45 +0900

    asd

---
+++ public/sw.js
@@ -0,0 +1,28 @@
+const cacheName = `serviceworker-0.6.18`;
+self.addEventListener('install', e => {
+    e.waitUntil(
+        caches.open(cacheName).then(cache => {
+            return cache.addAll([
+                `/`,
+                `/index.html`,
+                `/styles/main.css`,
+                `/scripts/main.min.js`,
+            ])
+                .then(() => self.skipWaiting());
+        })
+    );
+});
+
+self.addEventListener('activate', event => {
+    event.waitUntil(self.clients.claim());
+});
+
+self.addEventListener('fetch', event => {
+    event.respondWith(
+        caches.open(cacheName)
+            .then(cache => cache.match(event.request, { ignoreSearch: true }))
+            .then(response => {
+                return response || fetch(event.request);
+            })
+    );
+});

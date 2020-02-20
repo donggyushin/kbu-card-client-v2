10be32ecb88ce0a338d14cd7016e038d126ba944
shin donggyu <donggyu9410@gmail.com>
2020-02-21 03:30:22 +0900

    build file

---
+++ build/precache-manifest.c63b29e66399c87956d810c2a99c7bc9.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9be24f03ab6efd9a1dfad9f368fcd9c8",
+    "url": "/index.html"
+  },
+  {
+    "revision": "34d33e4dbcf8b57d3a8e",
+    "url": "/static/css/2.45753d6c.chunk.css"
+  },
+  {
+    "revision": "8ec7ab6cc34cca003251",
+    "url": "/static/css/main.aff96e94.chunk.css"
+  },
+  {
+    "revision": "34d33e4dbcf8b57d3a8e",
+    "url": "/static/js/2.e34343c2.chunk.js"
+  },
+  {
+    "revision": "f497f3e11143a56db5aa76a69a9dd8f3",
+    "url": "/static/js/2.e34343c2.chunk.js.LICENSE"
+  },
+  {
+    "revision": "8ec7ab6cc34cca003251",
+    "url": "/static/js/main.5a7b55ce.chunk.js"
+  },
+  {
+    "revision": "2bcb8084869f23f72706",
+    "url": "/static/js/runtime-main.88d4f3f8.js"
+  },
+  {
+    "revision": "b7c9e1e479de3b53f1e4e30ebac2403a",
+    "url": "/static/media/slick.b7c9e1e4.woff"
+  },
+  {
+    "revision": "ced611daf7709cc778da928fec876475",
+    "url": "/static/media/slick.ced611da.eot"
+  },
+  {
+    "revision": "d41f55a78e6f49a5512878df1737e58a",
+    "url": "/static/media/slick.d41f55a7.ttf"
+  },
+  {
+    "revision": "f97e3bbf73254b0112091d0192f17aec",
+    "url": "/static/media/slick.f97e3bbf.svg"
+  }
+]);
--- src/consts/endpoint.tsx
+++ src/consts/endpoint.tsx
@@ -1,10 +1,10 @@
-export const END_POINT = 'http://220.67.154.77:9766/'
-export const END_POINT_TEST = 'http://220.67.154.77:9766/'
+export const END_POINT = 'http://13.125.144.97:9766/'
+export const END_POINT_TEST = 'http://13.125.144.97:9766/'
 export let DONGGYU_END_POINT = 'http://localhost:4001/api/'
 
 if (!process.env.NODE_ENV || process.env.NODE_ENV === 'development') {
     // dev code
 } else {
     // production code
-    DONGGYU_END_POINT = 'https://kbucard.com:4001/api/'
+    DONGGYU_END_POINT = 'http://15.165.99.75:4001/api/'
 }
